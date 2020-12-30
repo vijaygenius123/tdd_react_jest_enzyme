423c8cc5e4bc65e7b60478fea4326014ae2a7f91
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-12-30 08:45:47 +0530

    Basic Testing Setup

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
 import App from './App';
+import { shallow } from 'enzyme'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('App', () => {
+  it('render without crashing', () => {
+    const wrapper = shallow(<App />);
+  })
+})
