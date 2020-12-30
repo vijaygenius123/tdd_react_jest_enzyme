9aadece14c95d464b62633666debc56724a78c92
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-12-30 13:37:30 +0530

    Added PersonList

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
 import App from './App';
+import PersonList from './components/PersonList'
 import { shallow } from 'enzyme'
 
 describe('App', () => {
   it('render without crashing', () => {
     const wrapper = shallow(<App />);
   })
+
+  it('renders a person list', () => {
+    const wrapper = shallow(<App />);
+    const personList = wrapper.find(PersonList);
+    expect(personList).toHaveLength(1);
+  })
+
 })
